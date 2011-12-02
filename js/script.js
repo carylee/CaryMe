4ec85b2115040d79287c1dfb3b483339c4ce0720
Cary Lee <carylee@gmail.com>
2011-12-02 11:23:14 -0600

    Added bio + twitter follow call on blog post, follow on blog index

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 */
 
-$(".btn.twitter").click(function(e){
+$(".btn.twitter, a.twitter").click(function(e){
   e.preventDefault();
   window.open(e.target.href, "_blank","toolbar=no, location=no, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=no, width=520, height=570");
 });
